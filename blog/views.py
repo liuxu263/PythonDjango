ab10fffc034bac2adf41b6d67ec118ca3bb398c8
liuxu <liuxu@xiaozhu.com>
2019-04-02 19:57:52 +0800

    up

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
+# ! /usr/bin/python
+# -*- coding:utf-8 -*-
+
+''
+
+__author__ = 'lx'
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'templates/blog/index.html', context={
+        'title': '我的博客首页',
+        'welcome': '欢迎访问我的博客首页'
+    })

c60533aa4349ed001e689ea4121fc98e6295b3c5
liuxu <liuxu@xiaozhu.com>
2019-04-03 11:12:02 +0800

    up

--- blog/views.py
+++ blog/views.py
@@ -7,10 +7,11 @@ __author__ = 'lx'
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 
 def index(request):
+    post_list = Post.objects.all().order_by('-created_time')
     return render(request, 'templates/blog/index.html', context={
-        'title': '我的博客首页',
-        'welcome': '欢迎访问我的博客首页'
+        'post_list': post_list
     })

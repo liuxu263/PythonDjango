9e591648ed4de13a594bc67da079dcff82a4a0ae
liuxu <liuxu@xiaozhu.com>
2019-07-11 16:51:35 +0800

    up

---
+++ PythonDjango/lx/LXUser/userviews.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+
+def loginin(request):
+    return render(request, )
---
+++ PythonDjango/lx/urls.py
@@ -0,0 +1,9 @@
+#! /usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+''
+
+__author__ = 'lx'
+
+from django.urls import path
+from . import views

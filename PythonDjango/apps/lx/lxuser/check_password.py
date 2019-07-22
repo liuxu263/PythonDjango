8cf912ebfe2d52da0cf4b6cf1b4ae6bfe2a5e1c9
liuxu <liuxu@xiaozhu.com>
2019-07-22 18:57:13 +0800

    up

--- PythonDjango/apps/lx/lxuser/check_password.py
+++ PythonDjango/apps/lx/lxuser/check_password.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 
 
-def check_password(request, password, password1):
-    check_password_length(request, password)
+def check_password(password, password1):
+    msg = 200
+    if check_password_length(password) is True:
+        msg = "密码过短"
+    return msg
 
 
-def check_password_length(request, password):
+def check_password_length(password):
     if len(password) <= 8:
-        print(len(password))
-        return False
+        return True
--- PythonDjango/apps/lx/lxuser/registerviews.py
+++ PythonDjango/apps/lx/lxuser/registerviews.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 from ..models import User
-from .check_password import check_password_length
+from .check_password import check_password
 
 
 def register(request):
     try:
+        msg = 200
         post = request.POST
         nickname = post.get('nickname')
         name = post.get('name')
         password = post.get('pwd1')
         password1 = post.get('pwd2')
-        if check_password_length(request, password) is False:
-            return render(request, 'register.html', {'msg': '注11'})
+        msg = check_password(password, password1)
         sex = post.get('sex')
         birthday = post.get('birthday')
         age = post.get('age')
@@ -26,6 +26,9 @@ def register(request):
                     birthday=birthday, age=age, IDCard=idcard, nation=nation, province=province,
                     city=city, county=county, phone=phone, email=email)
         user.save()
-        return render(request, 'homepage.html', {'msg': '注册成功'})
+        if msg != 200:
+            return render(request, 'register.html', {'msg': msg})
+        else:
+            return render(request, 'homepage.html', {'msg': "注册成功"})
     except:
         return render(request, 'register.html', {'msg': '注册失败'})
